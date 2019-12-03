0ce8240d5a7e9414cc21f95caafdbb3f5ed2eaac
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-12-03 09:45:28 +0100

    api

--- src/main/java/com/example/mobilecardtwo/MoblieCardEndpoint.java
+++ src/main/java/com/example/mobilecardtwo/MoblieCardEndpoint.java
@@ -1,14 +1,16 @@
 package com.example.mobilecardtwo;
 
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.property.StructuredName;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 
 @RestController
@@ -16,16 +18,62 @@ import java.io.IOException;
 public class MoblieCardEndpoint {
 
     @GetMapping("/search")
-    public ResponseEntity generateVCard(@RequestParam String searchText) throws IOException {
-        Document doc = Jsoup.connect("https://adm.edu.p.lodz.pl/user/users.php?search="+searchText).get();
+    public ResponseEntity searchUser(@RequestParam String searchPerson) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://adm.edu.p.lodz.pl/user/users.php?search=" + searchPerson;
 
-        Elements elements = doc.getElementsByClass("user-info");
-        elements.forEach(element -> System.out.println(element));
+        Document doc = Jsoup.connect(url).get();
+        Elements users = doc.select("div.user-info");
 
-        return ResponseEntity.ok().build();
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        String button = "<form action= \"DOMEN" +
+                        " \"> " +
+                        "<input type =\"button\" value = \"Generate VCard\">" +
+                        " </form>";
+
+        for (Element e : users) {
+            buffer.append(e.toString());
+            buffer.append("<a href=" + "/moblieCard/users/"+ searchPerson + "/generate/" + 1 + "><button>Generate Vcard</button></a>");
+        }
+        return ResponseEntity.ok(buffer.toString());
     }
 
-    @GetMapping("/generate"){
+    @GetMapping("/users/{personName}/generate/{id}")
+    public ResponseEntity generate(@PathVariable String personName, @PathVariable int id) throws IOException {
+
+        Document doc = Jsoup.connect("https://adm.edu.p.lodz.pl/user/users.php?search=" + personName).get();
+        Elements users = doc.select("div.user-info");
+
+        Elements name = users.select("h3").select("a");
+        Elements userInfo = users.select("span.item-content");
+
+        String currentName = null;
+        String info = null;
+
+        int i = 0;
+        for (Element e : name) {
+            if (i++ == id) {
+                currentName = e.text();
+                break;
+            }
+        }
+        i=0;
+        for (Element e : userInfo) {
+            if (i++ == id) {
+                info = e.text();
+                break;
+            }
+        }
+
+
+        VCard vcard = new VCard();
+        vcard.setFormattedName(currentName);
+        vcard.setOrganization(info);
+
+        String str = Ezvcard.write(vcard).version(VCardVersion.V4_0).go();
+
+        return ResponseEntity.ok(str);
 
     }
 
